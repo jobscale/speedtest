ddce7b8d3c3e3535dc055bb9c9d680201cfe0a5c
jobscale <jobscale@jsx.jp>
2019-05-26 20:39:20 +0900

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+require('modules');
+const { Shell } = require('shell');
+class SpeedTest {
+  constructor() {
+    this.cmd = 'speedtest-cli --simple --share';
+  }
+  run() {
+    return new Shell().exec(this.cmd)
+    .then(res => res)
+    .catch(e => e.message);
+  }
+}
+module.exports = {
+  SpeedTest,
+};
