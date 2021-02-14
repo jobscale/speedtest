c5b9d2d328534ee317c9d00933252cd1f3cad4dc
jobscale <jobscale@users.noreply.github.com>
2021-02-14 10:39:12 +0900

    speedtest-cli on docker

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const { Shell } = require('shell');
 class SpeedTest {
   constructor() {
     this.cmd = 'speedtest-cli --simple --share';
+    this.cmd = 'docker';
+    this.args = ['exec', '-i', 'node-aws', 'bash', '-c', '"speedtest-cli --simple --share"'];
   }
   run() {
-    return new Shell().exec(this.cmd)
+    return new Shell().spawn(this.cmd, this.args)
     .then(res => res)
     .catch(e => e.message);
   }
